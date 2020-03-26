c718fe8e6fce6a9a9fad6c68ca2602138c8c7f74
andre.sousa <andreg.sousa@hotmail.com>
2020-03-26 18:19:12 -0300

    Front end do app

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -18,14 +18,12 @@ module.exports = {
     async create(request, response) {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
-
         const [id] = await connection('incidents').insert({
             title,
             description,
             value,
             ong_id,
         });
-
         return response.json({ id });
     },
 
