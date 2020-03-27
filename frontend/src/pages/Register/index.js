66e8e69141b98521507a4281dbc918105b8ffdcf
andre.sousa <andreg.sousa@hotmail.com>
2020-03-26 21:58:30 -0300

    corrigir mensagem na tela de cadastro de ONG

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -45,7 +45,7 @@ export default function Register() {
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Não tenho cadastro
+                        Voltar para logon
                     </Link>
 
                 </section>
